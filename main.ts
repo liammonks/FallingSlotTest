624fa23bbd2a2e9354e6cc14324658ccbc9506bf
Liam Monks <liammonks1@gmail.com>
2021-09-15 17:39:01 +0100

    removed local fetch of pixi.js from index.html

--- main.ts
+++ main.ts
@@ -1,9 +1,5 @@
 import * as PIXI from 'pixi.js';
 
-const app = new PIXI.Application({
-    width: 400,
-    height: 400,
-    backgroundColor : 0x000000
-});
+const app = new PIXI.Application();
 
 console.log("created application");
